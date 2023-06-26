73e847f9d7fe7ffe4472039a00864bb32da8bf08
siesto1elemento <rohitojha9720@gmail.com>
2023-06-26 11:12:14 +0530

    Introduced template inheritance

--- tasks/tasksapp/views.py
+++ tasks/tasksapp/views.py
@@ -2,7 +2,11 @@ from django.shortcuts import render
 
 tasks = ["foo", "bar", "baz"]
 # Create your views here.
+
 def index(request):
     return render(request,"tasks/index.html",{
         "tasks": tasks
     })
+
+def add(request):
+    return render(request, "tasks/add.html")
